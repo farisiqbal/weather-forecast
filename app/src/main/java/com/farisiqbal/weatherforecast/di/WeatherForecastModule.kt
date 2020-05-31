fcbd4c96a7bc4a09d31c8abea45e38f4e0bb2a36
Faris Iqbal <faris.iqbal93@gmail.com>
2020-05-31 13:06:52 +0700

    downgrade mockk to 1.9.2, making koin works for now

--- app/src/main/java/com/farisiqbal/weatherforecast/di/WeatherForecastModule.kt
+++ app/src/main/java/com/farisiqbal/weatherforecast/di/WeatherForecastModule.kt
@@ -1,8 +1,10 @@
 package com.farisiqbal.weatherforecast.di
 
 import com.farisiqbal.weatherforecast.data.api.ApiService
+import com.farisiqbal.weatherforecast.data.repository.WeatherForecastRepository
 import com.farisiqbal.weatherforecast.data.repository.WeatherForecastRepositoryImpl
 import com.farisiqbal.weatherforecast.view.WeatherForecastViewModel
+import kotlinx.coroutines.Dispatchers
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,6 +13,7 @@ import org.koin.dsl.module
  */
 val weatherForecastModule = module {
     single { ApiService() }
-    single { WeatherForecastRepositoryImpl(get()) }
-    viewModel { WeatherForecastViewModel(get(), get()) }
+    // single { WeatherForecastRepository }
+    // single { WeatherForecastRepositoryImpl(get()) }
+    viewModel { WeatherForecastViewModel(WeatherForecastRepositoryImpl(get()), Dispatchers.Main) }
 }
